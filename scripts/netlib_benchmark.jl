953fbfc2a29fa337a57c9792043b3bd08a257739
mtanneau <mathieu.tanneau@gmail.com>
2019-04-09 18:12:46 -0400

    Update benchmark script

--- scripts/netlib_benchmark.jl
+++ scripts/netlib_benchmark.jl
@@ -1,59 +1,50 @@
 using Printf
 using Random
 
+using DataFrames
+using CSV
+
+using Statistics
 using LinearAlgebra
 BLAS.set_num_threads(1)
 
 import MathProgBase
 const MPB = MathProgBase
 
-
-import Clp:ClpSolver
-import ECOS:ECOSSolver
-import GLPK
-import GLPKMathProgInterface:GLPKSolverLP
-import Ipopt:IpoptSolver
-import Mosek:MosekSolver
 import Tulip
 
-function benchmark_instance(model::MPB.AbstractLinearQuadraticModel, res; verbose=false)
-    
-    res[:success] = false
-    res[:fail] = false
-    res[:error] = false
-    
-    try
-        t = @elapsed MPB.optimize!(model)
-        s = MPB.status(model)
-        
-        res[:time] = t
-        res[:status] = "$s"
-        # res[:iter] = MPB.getbarrieriter(model)
+DIRNAME = @__DIR__
 
-        if s == :Optimal
-            verbose && @printf "%8.2f s" t
-            res[:success] = true
-        else
-            verbose && @printf "%8.2f t" t
-            res[:fail] = true
-        end
-            
-    catch err
-        verbose && @printf "%8.2f f" 0Inf
-        # verbose && print(typeof(err))
-        res[:error] = true
-        res[:error_type] = "$(typeof(err))"
-        res[:time] = Inf
-    end
-    
-    flush(stdout)
-    
-    return
-end
+global const TIME_LIMIT = 300
 
-function benchmark(files, solvers, names; verbose=false)
+"""
+    gmean1(u)
+
+Compute the geometric mean with a shift of 1.
+"""
+gmean1(u) = exp(mean(log.(u .+ oneunit(eltype(u))))) - oneunit(eltype(u))
+
+# import solvers
+include("solvers.jl")
+
+# open-source solvers
+const SOLVERS_OS = [sCLP(), sECOS(), sGLPK(), sIPOPT(), sTLP()]
+const NAMES_OS   = solver_name.(SOLVERS_OS)
+
+# commercials solvers
+const SOLVERS_COMM = [sCPX(), sGRB(), sMSK()]
+const NAMES_COMM   = solver_name.(SOLVERS_COMM)
+
+"""
+    benchmark(files, solvers names; verbose)
+
+Benchmark the given solvers over the set of instances.
+"""
+function benchmark(files, solvers;
+    verbose=false
+)
     
-    @assert length(solvers) == length(names)
+    names = solver_name.(solvers)
     
     time_tot = 0.0  # Total solve time
 
@@ -80,13 +71,49 @@ function benchmark(files, solvers, names; verbose=false)
     
         for (s, name) in zip(solvers, names)
             
-            results[name][f] = Dict()
+            res = Dict()
             
             # Instanciate model
             model = MPB.LinearQuadraticModel(s)
             MPB.loadproblem!(model, A, collb, colub, c, b, b, :Min)
             
-            benchmark_instance(model, results[name][f], verbose=verbose)
+            res[:success] = false
+            res[:fail] = false
+            res[:error] = false
+            res[:status] = :Error
+            res[:time] = NaN
+            res[:error_type] = "None"
+            
+            try
+                t = @elapsed MPB.optimize!(model)
+                s = MPB.status(model)
+                
+                res[:time] = t
+                res[:status] = "$s"
+                # res[:iter] = MPB.getbarrieriter(model)
+
+                if s == :Optimal && t <= TIME_LIMIT
+                    # Instance is solved
+                    verbose && @printf "%10.3f" t
+                    res[:success] = true
+                else
+                    # Error or timeout
+                    verbose && @printf "%10s" "t"
+                    res[:time] = Float64(TIME_LIMIT)         # count failure as max time.
+                    res[:fail] = true
+                end
+                    
+            catch err
+                verbose && @printf "%10s" "f"
+                res[:error] = true
+                res[:error_type] = "$(typeof(err))"
+                res[:time] = Float64(TIME_LIMIT)
+            end
+            
+            flush(stdout)
+
+            results[name][f] = res
+    
         end
             
         verbose && @printf "\n"
@@ -97,94 +124,103 @@ function benchmark(files, solvers, names; verbose=false)
     return results
 end
 
+
 function main()
 
-    # Solvers
-    s_clp = ClpSolver(
-        LogLevel=0,
-        PresolveType=1,
-        SolveType=4,
-        MaximumSeconds=300
-    );
-
-    s_ecos = ECOSSolver(
-        verbose=0
-    )
-
-    s_glpk = GLPKSolverLP(
-    #   presolve=true,  # no pre-solve for interior-point (apparently)
-        method=:InteriorPoint,
-        msg_lev=0
-    );
-
-    s_ipo = IpoptSolver(
-        print_level=0,
-        max_cpu_time=300.0,
-    );
-
-    s_msk = MosekSolver(
-        MSK_IPAR_INTPNT_BASIS=0,
-        MSK_IPAR_NUM_THREADS=1,
-        MSK_IPAR_PRESOLVE_USE=0,
-        MSK_IPAR_LOG=0,
-        MSK_DPAR_OPTIMIZER_MAX_TIME=300
-    );
-
-    s_tlp = Tulip.TulipSolver(
-        verbose=0,
-        time_limit=300,
-        barrier_iter_max=200,
-        algo=1
-    );
-
-    solvers = [
-        # s_clp,
-        # s_ecos,
-        # s_glpk,
-        # s_ipo,
-        # s_msk,
-        s_tlp
-    ]
-    names = [
-        # :CLP,
-        # :ECOS,
-        # :GLPK,
-        # :IPO,
-        # :MSK,
-        :TLP
-    ]
-
+    # Open-source solvers
     # First compilation round
-    results = benchmark(
+    @info "Compilation rounds... This may take a few minutes"
+    benchmark(
         ["AFIRO.SIF"],
-        solvers,
-        names,
+        [SOLVERS_OS; SOLVERS_COMM],
+        verbose=false
+    );
+    # Compilation round for Tulip
+    benchmark(
+        readdir("../dat/netlib"),
+        [sTLP()],
         verbose=false
     );
 
     # Real deal
+    @info "Launching Netlib benchmark"
+    flush(stdout)
     results = benchmark(
         readdir("../dat/netlib"),
-        solvers,
-        names,
+        [SOLVERS_OS; SOLVERS_COMM],
         verbose=true
     );
 
-    for metric in [:time, :success]
-        @printf "%8s" "$metric"
-        for solver in names
-            @printf "%8.2f" total(results, metric, solver)
-        end
-        @printf "\n"
-    end
+    # println()
+    # for metric in [:time, :success]
+    #     @printf "%12s" "$metric"
+    #     for solver in names
+    #         @printf "%10.3f" total(results, metric, solver)
+    #     end
+    #     @printf "\n"
+    # end
 
-    return
+    # extract results
+    df = extract_results(results)
+
+    # write to CSV file
+    CSV.write("./res_netlib.csv", df)
+
+    return nothing
 end
 
 total(results, metric, solver) = sum([
     res[metric]
     for (k, res) in results[solver]
-        if haskey(res, metric) && res[metric] < Inf
+        if haskey(res, metric) && res[metric] < TIME_LIMIT
 ])
+
+"""
+    extract_results(res)
+
+Parse results and output csv files.
+"""
+function extract_results(res)
+
+    # Extract solvers
+    solvers = collect(keys(res))
+
+    # Extract problem names
+    pbnames_raw = collect(keys(res[solvers[1]]))
+    pbnames = lowercase.([f[1:end-4] for f in pbnames_raw])
+    p = sortperm(pbnames)
+    pbnames_raw = pbnames_raw[p]  # sort names
+
+    # extract metrics
+    metrics = collect(keys(res[solvers[1]][pbnames_raw[1]]))
+
+    # Create data frame with all results
+    df = DataFrame()
+    
+    df.instance = pbnames  # instance name
+    for s in solvers
+        for k in metrics
+            df[Symbol("$(k)_$(s)")] = [res[s][f][k] for f in pbnames_raw]
+        end
+    end
+
+    # convert boolean outputs to integers
+    for s in solvers
+        for k in metrics
+            if eltype(df[Symbol("$(k)_$(s)")]) == Bool
+                df[Symbol("$(k)_$(s)")] = Int.(df[Symbol("$(k)_$(s)")])
+            end
+        end
+    end
+
+    # Correct reported times
+    for s in solvers
+        t_ = df[Symbol("time_$s")]
+        succ_ = df[Symbol("success_$s")]
+        df[Symbol("time_$s")] .= min.(t_ , 300.0) .* succ_ .+ 300.0 .* (1.0 .- succ_)
+    end
+
+    return df
+end
 
 main()
---
+++ scripts/solvers.jl
@@ -0,0 +1,79 @@
+import Clp:ClpSolver
+import ECOS:ECOSSolver
+import GLPK
+import GLPKMathProgInterface:GLPKSolverLP
+import Ipopt:IpoptSolver
+import Tulip:TulipSolver
+
+import CPLEX:CplexSolver
+import Gurobi 
+import Gurobi:GurobiSolver
+import Mosek:MosekSolver
+
+global const GRBENV = Gurobi.Env()  # define this to avoid extra prints
+
+# Open-source solvers
+sCLP() = ClpSolver(
+    LogLevel=0,
+    PresolveType=1,
+    SolveType=4,
+    MaximumSeconds=TIME_LIMIT
+);
+
+sECOS() = ECOSSolver(
+    verbose=0
+)
+
+sGLPK() = GLPKSolverLP(
+#   presolve=true,  # no pre-solve for interior-point (apparently)
+    method=:InteriorPoint,
+    msg_lev=0,
+    TimeLimit=TIME_LIMIT
+);
+
+sIPOPT() = IpoptSolver(
+    print_level=0,
+    max_cpu_time=Float64(TIME_LIMIT),
+);
+
+sTLP() = Tulip.TulipSolver(
+    verbose=0,
+    time_limit=TIME_LIMIT,
+    barrier_iter_max=200,
+    algo=1
+);
+
+# Commercial solvers
+sCPX() = CplexSolver(
+    CPX_PARAM_SCRIND=0,
+    CPX_PARAM_PREIND=0,
+    CPX_PARAM_THREADS=1,
+    CPX_PARAM_LPMETHOD=4, # 0:default, 1:PS, 2:DS, 4:IPM, 6:Concurrent
+    CPX_PARAM_SOLUTIONTYPE=2  # 0: default, 1: basic, 2:no crossover,
+)
+
+sGRB() = GurobiSolver(
+    GRBENV,
+    OutputFlag=0,
+    Method=2,
+    Presolve=0,
+    Threads=1,
+    Crossover=0
+)
+
+sMSK() = MosekSolver(
+    MSK_IPAR_INTPNT_BASIS=0,
+    MSK_IPAR_NUM_THREADS=1,
+    MSK_IPAR_PRESOLVE_USE=0,
+    MSK_IPAR_LOG=0,
+    MSK_DPAR_OPTIMIZER_MAX_TIME=TIME_LIMIT
+);
+
+solver_name(::ClpSolver) = :CLP
+solver_name(::ECOSSolver) = :ECOS
+solver_name(::GLPKSolverLP) = :GLPK
+solver_name(::IpoptSolver) = :IPOPT
+solver_name(::TulipSolver) = :TLP
+solver_name(::CplexSolver) = :CPX
+solver_name(::GurobiSolver) = :GRB
+solver_name(::MosekSolver) = :MSK
